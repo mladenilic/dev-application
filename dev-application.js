8acaf7cc5188b2b30458fd98d65caec325ea6e3c
Mladen Ilic <mladenilic480@gmail.com>
2015-12-22 22:09:19 +0100

    Implement message submodule

--- dev-application.js
+++ dev-application.js
@@ -1,7 +1,7 @@
 var DevApplication = (function () {
-	var _canInit = true;
+    var _canInit = true;
 
-	var _options = {
+    var _options = {
         messages: {
             welcome: {
                 title: '<3 web development? start a DevApplication',
@@ -16,63 +16,105 @@ var DevApplication = (function () {
                 ]
             }
         },
-		styles: {
-			fontSize: 15,
-            colors: {
+        styles: {
+            size: {
+                title: '20px',
+                default: '15px'
+            },
+            color: {
                 title: '#ff6f00',
                 error: '#f00',
                 success: '#0f0',
                 default: '#333'
             }
-		}
-	};
+        }
+    };
 
-	var _write = function (text, color, size) {
-		console.log('%c' + text, ['color: ', color, '; font-size: ', size, 'px;'].join(''));
-	};
+    var Message = (function () {
+
+        var Message = function () {
+            var _text = '', styles = {};
+
+            this.print = function () {
+                var computedStyles = '';
+                for (var i in styles) {
+                    computedStyles += i + ': ' + styles[i] + ';';
+                }
+
+                console.log('%c' + _text, computedStyles);
+
+                return this;
+            };
+
+            this.text = function (text) {
+                _text = text;
+
+                return this;
+            };
+
+            this.style = function (key, value) {
+                styles[key] = value;
+
+                return this;
+            };
+        };
+
+        return {
+            new: function () {
+                return new Message();
+            }
+        }
+
+    })();
 
     var _printTitle = function (title) {
-        _write(title, _options.styles.colors.title, parseInt(_options.styles.fontSize  * 1.3));
+        Message.new()
+            .text(title)
+            .style('color', _options.styles.color.title)
+            .style('font-size', _options.styles.size.title)
+            .print();
     };
 
     var _printMessages = function (messages) {
+        var message = Message.new()
+            .style('color', _options.styles.color.default)
+            .style('font-size', _options.styles.size.default);
+
         for (var i = 0; i < messages.length; i++) {
-            _write(messages[i], _options.styles.colors.default, _options.styles.fontSize);
+            message.text(messages[i]).print();
         };
     };
 
-	var _printError = function () {
+    var _printError = function () {
         var errorMessages = _options.messages.error.default;
-		_write(
-			errorMessages[Math.floor(Math.random() * errorMessages.length)],
-			_options.styles.colors.error,
-            _options.styles.fontSize
-		);
-	}
+        Message.new().text(errorMessages[Math.floor(Math.random() * errorMessages.length)])
+            .style('color', _options.styles.color.error)
+            .style('font-size', _options.styles.color.default)
+            .print();
+    }
 
-	return {
-		init: function (options) {
-			if (!_canInit) {
-				_printError();
-				return;
-			}
+    return {
+        init: function (options) {
+            if (!_canInit) {
+                _printError();
+                return;
+            }
 
-			for (var i in options) {
-				_options[i] = options[i];
-			}
+            for (var i in options) {
+                _options[i] = options[i];
+            }
 
-			_printTitle(_options.messages.welcome.title);
+            _printTitle(_options.messages.welcome.title);
             _printMessages(_options.messages.welcome.content);
-			console.log(this);
+            console.log(this);
 
-			_canInit = false;
-		},
-		about: function () {
-			_printMessages(_options.messages.about);
-		},
-		apply: function () {
+            _canInit = false;
+        },
+        about: function () {
+            _printMessages(_options.messages.about);
+        },
+        apply: function () {
 
-		}
-	};
-
+        },
+    };
 })();

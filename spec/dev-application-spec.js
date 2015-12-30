a10d12d661a98b8b8acea7628fd5af68c6be3f7f
Mladen Ilic <mladenilic480@gmail.com>
2015-12-30 17:07:54 +0100

    Create tests

--- spec/dev-application-spec.js
+++ spec/dev-application-spec.js
@@ -1,13 +1,37 @@
 'use strict';
 
 describe('Dev application spec', function() {
-  var application
+  var TestApplication;
 
   beforeEach(function () {
-    application = new DevApplication();
+    TestApplication = new DevApplication();
   });
 
-  it('should not throw on about()', function() {
-    expect(application.about).not.toThrow();
+  it('should not be able to print about', function() {
+    expect(TestApplication.about()).toBe(true);
+  });
+
+  it('should not be able to start application', function() {
+    expect(TestApplication.start()).toBe(true);
+  });
+
+  describe('when application has been started', function () {
+    beforeEach(function () {
+        TestApplication.start();
+    });
+
+    it('should be able to add cv', function () {
+        expect(TestApplication.addCv('http://example-cv.com')).toBe(true);
+    });
+
+    describe('when cv has been added', function () {
+        beforeEach(function () {
+            TestApplication.addCv('http://example-cv.com');
+        });
+
+        it('should be able to finish application', function () {
+            expect(TestApplication.finish()).toBe(true);
+        });
+    });
   });
 });

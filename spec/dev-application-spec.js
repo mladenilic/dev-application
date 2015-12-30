8c796255c4a9acfff229e1def59054697b091297
Mladen Ilic <mladenilic480@gmail.com>
2015-12-30 14:43:03 +0100

    Update spec

--- spec/dev-application-spec.js
+++ spec/dev-application-spec.js
@@ -1,5 +1,11 @@
-describe('DevApplication', function() {
-  var application = new DevApplication();
+'use strict';
+
+describe('Dev application spec', function() {
+  var application
+
+  beforeEach(function () {
+    application = new DevApplication();
+  });
 
   it('should not throw on about()', function() {
     expect(application.about).not.toThrow();

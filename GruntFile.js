e656f3073d77cc68fef69a70b0cbecba0676cd33
Mladen Ilic <mladenilic480@gmail.com>
2015-12-30 13:47:01 +0100

    Add jasmine grunt task

--- GruntFile.js
+++ GruntFile.js
@@ -1,19 +1,27 @@
 module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
         watch: {
             files: 'src/<%= pkg.name %>.js',
-            tasks: ['jshint'],
+            tasks: ['jshint', 'jasmine'],
         },
 
         jshint: {
             options: {jshintrc: '.jshintrc'},
             src: 'src/<%= pkg.name %>.js'
-        }
+        },
+
+        jasmine : {
+            src : 'src/src/<%= pkg.name %>.js',
+            options : {
+                specs : 'spec/src/<%= pkg.name %>-spec.js'
+            }
+        },
     });
 
     grunt.registerTask('default', 'watch');
